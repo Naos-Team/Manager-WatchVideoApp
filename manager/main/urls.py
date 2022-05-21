dbbb5cefd46d3583f03367311ab6a99390a1caf6
Do Thanh Tuan <81170150+tuanit1@users.noreply.github.com>
2022-05-21 17:28:04 +0700

    commit

--- manager/main/urls.py
+++ manager/main/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-
+app_name = 'main'
 urlpatterns = [
-    path('', views.main_view),
+    path('', views.login_view),
+    path('/logout', views.logoutPage, name='logout'),
 ]
--- manager/main/views.py
+++ manager/main/views.py
@@ -1,4 +1,35 @@
 from django.shortcuts import render
+import re
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 
-def main_view(request):
-    return render(request, 'main.html')
+
+def login_view(request):
+    if(request.user.is_authenticated):
+        return render(request, 'main.html')
+
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return render(request, 'main.html')
+            else:
+                messages.error(request, "Wrong password!")
+        except:
+            messages.error(request, "User is not exist!")
+
+    context = {}
+
+    return render(request, 'login.html', context)
+
+def logoutPage(request):
+    logout(request)
+    return redirect('/')
