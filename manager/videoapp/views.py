23e3bb45aec4f9f75c1dab469859425a23707c92
Do Thanh Tuan <81170150+tuanit1@users.noreply.github.com>
2022-05-24 01:55:58 +0700

    tuan cmm

--- manager/videoapp/views.py
+++ manager/videoapp/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from main.models import TblVideo
+from dateutil.parser import parse
+from main.models import TblVideo, TblCategory
 # Create your views here.
 
 
@@ -12,7 +13,15 @@ def updateVideo(request, id):
 
     video = TblVideo.objects.get(vid_id=id)
 
-    context = {'video': video}
+    date_time = video.vid_time.strftime("%Y-%m-%d")
+
+    categories = TblCategory.objects.filter(cat_type=1)
+
+    context = {
+        'video': video, 
+        'date': date_time,
+        'categories': categories
+    }
 
     return render(request, 'videoapp/updateVideo.html', context)
 
