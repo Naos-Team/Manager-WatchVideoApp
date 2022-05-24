fd5c836eb96d75de71ec1251d8a6dd9dc7516b36
Tuyen <dttuyen260701@gmail.com>
2022-05-24 18:42:30 +0700

    Tuyen commit: done add, edit Radio TV

--- manager/main/base64_change.py
+++ manager/main/base64_change.py
@@ -2,7 +2,7 @@ import base64
 
 
 def encode_Str(values):
-    return base64.b64encode(values.encode('ascii')).decode('ascii')
+    return base64.b64encode(values.encode('utf-8')).decode('utf-8')
     
 def decode_Str(values):
-    return base64.b64decode(values).decode('ascii')
+    return base64.b64decode(values).decode('utf-8')
