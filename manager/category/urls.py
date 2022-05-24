7f9001e35ee1ead851065fae83c0ce553d2a902e
Tuyen <dttuyen260701@gmail.com>
2022-05-24 12:51:18 +0700

    Tuyen commit: done Category wait link img

--- manager/category/urls.py
+++ manager/category/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'category'
 urlpatterns = [
     path('category/<str:pk>/', views.category, name='category'),
+    path('category-add/<str:pk>/', views.addCategory, name='add'),
+    path('category-edit/<str:pk>/<str:id>', views.editCategory, name='edit'),
     path('category/<str:pk>/<str:id>', views.disableCategory, name='delcate'),
 ]
--- manager/category/views.py
+++ manager/category/views.py
@@ -7,23 +7,68 @@ from django.contrib.auth.decorators import login_required
 import base64
 # Create your views here.
 
+def encode_Str(columns):
+    return base64.b64encode(columns.encode('ascii')).decode('ascii')    
+
+def decode_Item_cate(category):
+    category.cat_name = base64.b64decode(category.cat_name)
+    category.cat_name = category.cat_name.decode('ascii')
+    category.cat_image = base64.b64decode(category.cat_image)
+    category.cat_image = category.cat_image.decode('ascii')
+    return category
+
 def decode_Cate(categories):
     for category in categories:
-        category.cat_name = base64.b64decode(category.cat_name)
-        category.cat_name = category.cat_name.decode('ascii')
-        category.cat_image = base64.b64decode(category.cat_image)
-        category.cat_image = category.cat_image.decode('ascii')
+        category = decode_Item_cate(category)
     return categories
 
 def category(request , pk):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    q_encode = base64.b64encode(q.encode('ascii')).decode('ascii') if request.GET.get('q') != None else ''
+    q_encode = encode_Str(q) if request.GET.get('q') != None else ''
     categories = TblCategory.objects.filter(
         Q(cat_type=pk) &
-        Q(cat_name__icontains= q_encode)
+        Q(cat_name__icontains= q_encode) |
+        Q(cat_id__icontains= q)
         )
     context = {'categories':decode_Cate(categories), 'choice':pk}
     return render(request, 'category/category.html', context)
+
+@login_required(login_url='/login')
+def addCategory(request, pk):
+    if request.method == 'POST':
+        category_new = {'cat_name':encode_Str(request.POST.get('Name')), 
+        'cat_image':encode_Str('https://vcdn1-dulich.vnecdn.net/2020/11/21/1-1605972569.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=Gs5VBQ9eOvNSi6tNWUhkRQ'),
+        'cat_type':pk, 'cat_status':request.POST.get('status')}
+        form = CategoryForm(category_new)
+        if form.is_valid():
+            form.save()
+            return redirect('category:category', pk)
+        else:
+            return HttpResponse("Error")
+    else:
+        page = 'add'
+        context = {'page': page, 'choice':pk}
+        return render(request, 'category/categorydetail.html', context)
+
+@login_required(login_url='/login')
+def editCategory(request, pk, id):
+    category = TblCategory.objects.get(cat_id = id)
+    category = decode_Item_cate(category)
+    if request.method == 'POST':
+        category_new = {'cat_id':category.cat_id,
+        'cat_name':encode_Str(request.POST.get('Name')), 
+        'cat_image':encode_Str('https://media.bongda.com.vn/resize/800x800/files/news/2019/06/23/torres-tiet-lo-ly-do-giai-nghe-chi-ra-cau-thu-hay-nhat-tung-choi-cung-152947.jpg'),
+        'cat_type':category.cat_type, 'cat_status':request.POST.get('status')}
+        form = CategoryForm(category_new, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('category:category', pk)
+        else:
+            return HttpResponse("Error")
+    else:
+        page = 'edit'
+        context = {'page': page, 'choice':pk, 'category':category}
+        return render(request, 'category/categorydetail.html', context)
 
 @login_required(login_url='/login')
 def disableCategory(request, pk, id):
