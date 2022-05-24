e91c1d4d1ef7e21180d43f2bd2f0f894d2a3c59c
yennhiluu2112 <yennhiluu2112@gmail.com>
2022-05-24 14:40:39 +0700

    nhi cm

--- manager/comment/views.py
+++ manager/comment/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
-from main.models import TblReport, TblVideo
+from main.models import TblComment, TblReport, TblVideo
 from report.forms import ReportForm
 from django.db.models import Q
 import base64
@@ -15,17 +15,17 @@ import base64
 def report_home(request, video_type):
     video_search = request.GET.get('tv_search_video') if request.GET.get('tv_search_video') != None else ''
     if (video_search=="" and video_type==0):
-        videos = TblVideo.objects.all()
+        comments = TblComment.objects.all()
     elif (video_search==""):
-        videos = TblVideo.objects.filter(vid_type = video_type)
+        comments = TblComment.objects.filter(vid__vid_type = video_type)
     else:
         if video_search.isnumeric():
-            videos = TblVideo.objects.filter(Q(vid_id = video_search) | Q(vid_type = video_type) )
+            comments = TblComment.objects.filter(Q(vid__vid_id = video_search) | Q(vid__vid_type = video_type) )
         else:
             if video_search.lower() == 'all':
-                videos = TblVideo.objects.all()
+                comments = TblComment.objects.all()
             else:
-                videos = TblVideo.objects.filter(Q(vid_title = video_search) | Q(vid_type = video_type) )
+                comments = TblComment.objects.filter(Q(vid__vid_title = video_search) | Q(vid__vid_type = video_type) )
 
-    context = {'videos': videos, 'video_type':video_type}
+    context = {'comments': comments, 'video_type':video_type}
     return render(request, 'comment/home_comment.html', context)
