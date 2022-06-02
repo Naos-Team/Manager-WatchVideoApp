54cd98ccb91dbe96d5c0e9fedb5d98d3a420ac95
yennhiluu2112 <yennhiluu2112@gmail.com>
2022-06-02 13:03:31 +0700

    nhi cm: done api report

--- manager/comment/views.py
+++ manager/comment/views.py
@@ -12,6 +12,9 @@ from django.db.models import Q
 import base64
 import main.base64_change as bs
 from django.core.paginator import Paginator
+from Constant import SERVER_URL
+import json
+import requests
 
 def decode_Item_Cmt(comment):
     comment.uid.uid = bs.decode_Str(comment.uid.uid)
@@ -39,100 +42,54 @@ def decode_Vid(videos):
 
 
 def comment_home(request, video_type):
-    # video_search = request.GET.get('tv_search_video') if request.GET.get('tv_search_video') != None else ''
-    # if (video_search=="" and video_type==0):
-    #     comments = TblVideo.objects.all()
-    # elif (video_search==""):
-    #     comments = TblVideo.objects.filter(vid__vid_type = video_type)
-    # else:
-    #     if video_search.isnumeric():
-    #         comments = TblVideo.objects.filter(Q(vid__vid_id = video_search) | Q(vid__vid_type = video_type) )
-    #     else:
-    #         if video_search.lower() == 'all':
-    #             comments = TblVideo.objects.all()
-    #         else:
-    #             comments = TblVideo.objects.filter(Q(vid__vid_title = video_search) | Q(vid__vid_type = video_type) )
-
-    # context = {'comments': comments, 'video_type':video_type}
-    # return render(request, 'comment/home_comment.html', context)
-
     video_search = request.GET.get('tv_search_video') if request.GET.get('tv_search_video') != None else ''
-    if (video_search=="" and video_type==0):
-        videos = TblVideo.objects.all()
-    elif (video_search==""):
-        videos = TblVideo.objects.filter(vid_type = video_type)
-    else:
-        if video_search.isnumeric():
-            videos = TblVideo.objects.filter(Q(vid_id = video_search) & Q(vid_type = video_type) )
-        else:
-            if video_search.lower() == 'all':
-                videos = TblVideo.objects.all()
-            else:
-                videos = TblVideo.objects.filter(Q(vid_title = video_search) & Q(vid_type = video_type) )
+    postObj = {
+        'method_name': 'LOAD_ALL_VID_CMT',
+        'vid_search': video_search,
+        'vid_type': video_type,
+    }
+    data = {
+        'data': json.dumps(postObj),
+    }
+    res = requests.post(SERVER_URL, data=data)
+    return_object = json.loads(res.content)
 
-    cmt_count =  []
-    videos_de = decode_Vid(videos)
+    list_vid = return_object['list']
 
-    for video in videos:
-        cmts = TblComment.objects.filter(vid = video)
-        cmt_count.append(len(cmts))
+    # #Paginator
+    # p = Paginator(list_vid, 8)
+    # page = request.GET.get('page') 
+    # list_vids = p.get_page(page)
+    # nums = "a" * list_vids.paginator.num_pages
 
-    temp = []
-    list_vid = []
-    for i in range(0,len(videos_de)):
-        temp__ = []
-        temp__.append(videos_de[i])
-        temp__.append(cmt_count[i])
-        temp.append(temp__)
-
-    for x,y in temp:
-        temp_ = []
-        if y!=0:
-            temp_.append(x)
-            temp_.append(y)
-            list_vid.append(temp_)
-
-    #Paginator
-    p = Paginator(list_vid, 8)
-    page = request.GET.get('page') 
-    list_vids = p.get_page(page)
-    nums = "a" * list_vids.paginator.num_pages
-
-    context = {'list_vid': list_vid, 'video_type':video_type, 'list_vids':list_vids, 'nums':nums}
+    # context = {'list_vid': list_vid, 'video_type':video_type,}
     return render(request, 'comment/home_comment.html', context)
 
 @login_required(login_url='/login')
 def comment_main(request, video_id):
     cmt_search = request.GET.get('tv_search_cmt') if request.GET.get('tv_search_cmt') != None else ''
-    if (cmt_search==""):
-        cmts = TblComment.objects.filter(vid__vid_id = video_id)
-    else:
-        if cmt_search.isnumeric():
-            cmts = TblComment.objects.filter(Q(cmt_id = cmt_search) & Q(vid__vid_id = video_id))
-        else:
-            if cmt_search.lower() == 'all':
-                cmts = TblComment.objects.all()
-            else:
-                cmt_search_en = bs.encode_Str(cmt_search)
-                cmts = TblComment.objects.filter(Q(uid__uid__icontains = cmt_search_en) | Q(cmt_text = cmt_search_en) & Q(vid__vid_id = video_id)) 
+    postObj = {
+        'method_name': 'LOAD_CMT_BY_VID_SEARCH',
+        'vid_id': video_id,
+        'cmt_search': cmt_search,
+    }
+    data = {
+        'data': json.dumps(postObj),
+    }
+    res = requests.post(SERVER_URL, data=data)
+    return_object = json.loads(res.content)
 
-    video = get_object_or_404(TblVideo, vid_id = video_id)
-    cmts_de = decode_Cmt(cmts)
-
-    p = Paginator(cmts_de, 8)
-    page = request.GET.get('page') 
-    list_cmts = p.get_page(page)
-    nums = "a" * list_cmts.paginator.num_pages
+    reports = return_object['list']
 
     context = {
-        'cmts': cmts_de, 
-        'video_id' : video_id, 
-        'video_cat' : bs.decode_Str(video.cat.cat_name), 
-        'video_title' : bs.decode_Str(video.vid_title), 
-        'video_view': video.vid_view, 
-        'num_cmts': len(cmts),
-        'nums': nums,
-        'list_cmts': list_cmts,
+        # 'cmts': cmts_de, 
+        # 'video_id' : video_id, 
+        # 'video_cat' : bs.decode_Str(video.cat.cat_name), 
+        # 'video_title' : bs.decode_Str(video.vid_title), 
+        # 'video_view': video.vid_view, 
+        # 'num_cmts': len(cmts),
+        # 'nums': nums,
+        # 'list_cmts': list_cmts,
     }
     return render(request, 'comment/list_comment.html', context)
 
--- manager/report/views.py
+++ manager/report/views.py
@@ -1,5 +1,6 @@
+from telnetlib import SE
 from unicodedata import unidata_version
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
@@ -11,10 +12,13 @@ from report.forms import ReportForm
 from django.db.models import Q
 import main.base64_change as bs
 from django.core.paginator import Paginator
+from Constant import SERVER_URL
+import json
+import requests
 
 def decode_Item_Report(report):
-    report.uid.uid = bs.decode_Str(report.uid.uid)
-    report.report_content = bs.decode_Str(report.report_content)
+    report['uid']= bs.decode_Str(report['uid'])
+    report['report_content'] = bs.decode_Str(report['report_content'])
     return report
 
 def decode_Report(reports):
@@ -23,10 +27,7 @@ def decode_Report(reports):
     return reports
 
 def decode_Item_Vid(video):
-    video.vid_title = bs.decode_Str(video.vid_title)
-    video.vid_url = bs.decode_Str(video.vid_url)
-    video.vid_thumbnail = bs.decode_Str(video.vid_thumbnail)
-    video.vid_description = bs.decode_Str(video.vid_description)
+    video['vid_title'] = bs.decode_Str(video['vid_title'])
     return video
 
 def decode_Vid(videos):
@@ -37,113 +38,108 @@ def decode_Vid(videos):
 
 def report_home(request, video_type):
     video_search = request.GET.get('tv_search_video') if request.GET.get('tv_search_video') != None else ''
-    if (video_search=="" and video_type==0):
-        videos = TblVideo.objects.all()
-    elif (video_search==""):
-        videos = TblVideo.objects.filter(vid__vid_type = video_type)
-    else:
-        if video_search.isnumeric():
-            videos = TblVideo.objects.filter(Q(vid__vid_id = video_search) & Q(vid__vid_type = video_type) )
-        else:
-            if video_search.lower() == 'all':
-                videos = TblVideo.objects.all()
-            else:
-                videos = TblVideo.objects.filter(Q(vid__vid_title = video_search) & Q(vid__vid_type = video_type) )
-    
-    re_count =  []
-    temp = []
-    videos_de = decode_Vid(videos)
+    postObj = {
+        'method_name': 'LOAD_ALL_VID_REPORT',
+        'vid_search': video_search,
+        'vid_type': video_type,
+    }
+    data = {
+        'data': json.dumps(postObj),
+    }
+    res = requests.post(SERVER_URL, data=data)
+    return_object = json.loads(res.content)
 
-    for video in videos:
-        reports = TblReport.objects.filter(vid = video)
-        re_count.append(len(reports))
+    list_vid = decode_Vid(return_object['list'])
 
-    list_vid = []
-    for i in range(0,len(videos_de)):
-        temp__ = []
-        temp__.append(videos_de[i])
-        temp__.append(re_count[i])
-        temp.append(temp__)
-
-    for x,y in temp:
-        temp_=[]
-        if y!=0:
-            temp_.append(x)
-            temp_.append(y)
-            list_vid.append(temp_)
-
-    #Paginator
+    Paginator
     p = Paginator(list_vid, 8)
     page = request.GET.get('page') 
     list_vids = p.get_page(page)
     nums = "a" * list_vids.paginator.num_pages
 
-    context = {'list_vid':list_vid, 'video_type':video_type, 'list_vids':list_vids, 'nums': nums}
+    context = {'list_vids':list_vids, 'video_type':video_type, 'nums':nums}
     return render(request, 'report/home_report.html', context)
 
 @login_required(login_url='/login')
 def report_main(request, video_id):
     report_search = request.GET.get('tv_search_report') if request.GET.get('tv_search_report') != None else ''
-    if (report_search==""):
-        reports = TblReport.objects.filter(vid__vid_id = video_id)
-    else:
-        if report_search.isnumeric():
-            reports = TblReport.objects.filter(Q(report_id = report_search) & Q(vid__vid_id = video_id))
-        else:
-            if report_search.lower() == 'all':
-                reports = TblReport.objects.all()
-            else:
-                reports = TblReport.objects.filter(Q(uid = report_search) & Q(vid__vid_id = video_id))   
-    i = len(reports)
-    if i == 1 or i == 0:
-        num_reports = str(i) + ' Report Here';
-    else:
-        num_reports = str(i) + ' Reports Here';
+    postObj = {
+        'method_name': 'LOAD_REPORT_BY_VID',
+        'vid_id': video_id,
+        'report_search': bs.encode_Str(report_search.lower()),
+    }
+    data = {
+        'data': json.dumps(postObj),
+    }
+    res = requests.post(SERVER_URL, data=data)
+    return_object = json.loads(res.content)
 
-    #Paginator
+    reports = decode_Report(return_object['list'])
+
+    Paginator
     p = Paginator(reports, 8)
     page = request.GET.get('page') 
     list_res = p.get_page(page)
     nums = "a" * list_res.paginator.num_pages
 
-    context = {'reports': decode_Report(reports), 'video_id':video_id, 'num_reports':num_reports , 'list_res':list_res, 'nums': nums }
+    context = {'video_id':video_id, 'list_res':list_res, 'nums': nums }
     return render(request, 'report/list_report.html', context)
 
 @login_required(login_url='/login')
 def detail_report(request, report_id):
-    report = get_object_or_404(TblReport, report_id=report_id)
-    reportid_de = report.report_id
-    uid_de = bs.decode_Str(report.uid.uid)
-    vid_de = report.vid.vid_id
-    report_content_de =  bs.decode_Str(report.report_content)
-    print(report_content_de)
-    report_status_de =  report.report_status
+    postObj = {
+        'method_name': 'LOAD_DETAIL_REPORT',
+        'report_id': report_id,
+    }
+    data = {
+        'data': json.dumps(postObj),
+    }
+    res = requests.post(SERVER_URL, data=data)
+    return_object = json.loads(res.content)
+    report = decode_Report(return_object['list'])
+    report_id_de = report[0]['report_id']
+    report_content_de = report[0]['report_content']
+    report_status_de = report[0]['report_status']
+    vid_de = report[0]['vid_id']
+    uid_de = report[0]['uid']
 
-    if request.method=="POST":
+    if request.method=="POST": 
         report_id = request.POST.get('report_id')
         uid = request.POST.get('uid')
         vid = request.POST.get('vid')
         report_content = request.POST.get('report_content')
         report_status = request.POST.get('report_status')
+        postObj = {
+            'method_name': 'UPDATE_REPORT',
+            'report_id': report_id,
+            'uid': bs.encode_Str(uid),
+            'vid_id': vid,
+            'report_content': bs.encode_Str(report_content),
+            'report_status': report_status, 
+        }
+        data = {
+            'data': json.dumps(postObj),
+        }
+        res = requests.post(SERVER_URL, data=data)
+        return_object = json.loads(res.content)
+        return redirect('/report/'+str(vid)+'/report_main')
 
-        uid_en = bs.encode_Str(uid)
-        report_content_en = bs.encode_Str(report_content)
-
-        context1 = { 'report_id':report_id, 'uid':uid_en, 'vid':vid, 'report_content':report_content_en, 'report_status':report_status}
-        form = ReportForm(context1, instance=report)
-        if form.is_valid():
-            form.save()
-            return redirect('/report/'+str(vid)+'/report_main')
-
-    context = { 'report_id':reportid_de, 'uid':uid_de, 'vid':vid_de, 'report_content':report_content_de, 'report_status':report_status_de }
+    context = { 'report_id':report_id_de, 'uid':uid_de, 'vid':vid_de, 'report_content':report_content_de, 'report_status':report_status_de }
 
     return render(request, 'report/detail_report.html', context)
 
 @login_required(login_url='/login')
 def delete_report(request, report_id):
-    report = get_object_or_404(TblReport, report_id=report_id)
     if request.method=="POST":
-        report.delete()
+        postObj = {
+            'method_name': 'DELETE_REPORT',
+            'report_id': report_id,
+        }
+        data = {
+            'data': json.dumps(postObj),
+        }
+        res = requests.post(SERVER_URL, data=data)
+        return_object = json.loads(res.content)
     return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
 
     
