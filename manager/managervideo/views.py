1b7d42fdc74893c00900e00ed54ab971adaa7e7c
Tuyen <dttuyen260701@gmail.com>
2022-06-03 00:31:27 +0700

    Tuyen commit: done cai gi do

--- manager/managervideo/views.py
+++ manager/managervideo/views.py
@@ -1,4 +1,5 @@
 import json
+from webbrowser import get
 import requests as rq
 from django.contrib import messages
 from datetime import datetime
@@ -12,11 +13,17 @@ from .videoform import VideoForm
 from datetime import date
 import base64
 from django.core.paginator import Paginator
+from Constant import SERVER_URL
+import requests
+import json
 # Create your views here.
 
 def decode_Item_cate(category):
-    category.cat_name = bs.decode_Str(category.cat_name)
-    category.cat_image = bs.decode_Str(category.cat_image)
+    category['cat_id'] = int(category['cat_id'])
+    category['cat_name'] = bs.decode_Str(category['cat_name'])
+    category['cat_image'] = bs.decode_Str(category['cat_image'])
+    category['cat_type'] = int(category['cat_type'])
+    category['cat_status'] = int(category['cat_status'])
     return category
 
 def decode_Cate(categories):
@@ -25,10 +32,18 @@ def decode_Cate(categories):
     return categories
 
 def decode_Item_Video(video):
-    video.vid_title = bs.decode_Str(video.vid_title)
-    video.vid_url = bs.decode_Str(video.vid_url)
-    video.vid_thumbnail = bs.decode_Str(video.vid_thumbnail)
-    video.vid_description = bs.decode_Str(video.vid_description)
+    video['vid_id'] = int(video['vid_id'])
+    video['cat_id'] = int(video['cat_id'])
+    video['vid_title'] = bs.decode_Str(video['vid_title'])
+    video['vid_url'] = bs.decode_Str(video['vid_url'])
+    video['vid_thumbnail'] = bs.decode_Str(video['vid_thumbnail'])
+    video['vid_description'] = bs.decode_Str(video['vid_description'])
+    video['vid_view'] = int(video['vid_view'])
+    video['vid_duration'] = int(video['vid_duration'])
+    video['vid_avg_rate'] = float(video['vid_avg_rate'])
+    video['vid_status'] = int(video['vid_status'])
+    video['vid_type'] = int(video['vid_type'])
+    video['vid_is_premium'] = int(video['vid_is_premium'])
     return video
 
 def decode_Video(videos):
@@ -36,27 +51,44 @@ def decode_Video(videos):
         video = decode_Item_Video(video)
     return videos
 
+def getVid(id):
+    postObj = {
+        'method_name': 'GET_VIDEO_BYID',
+        'vid_id': id
+    }
+
+    data = {
+        'data': json.dumps(postObj)
+    }
+
+    res = requests.post(SERVER_URL , data = data)
+    return_obj = json.loads(res.content)
+
+    return return_obj['video'] if return_obj['status'] == "success" else []
+
 def managervideo(request , pk, cat):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    categories = TblCategory.objects.filter(Q(cat_type=pk))
-    categories = decode_Cate(categories)
 
-    if cat != 0:
-        videos = TblVideo.objects.filter( 
-            Q(vid_type=pk) &
-            Q(cat__cat_id=cat)
-        )
-    else:
-          videos = TblVideo.objects.filter(
-            Q(vid_type=pk)
-        )
+    postObj = {
+        'method_name': 'LOAD_TV_OR_RADIO',
+        'type':pk,
+        'cate_id':cat,
+        'search_txt': q
+    }
+
+    data = {
+        'data': json.dumps(postObj)
+    }
+
+    res = requests.post(SERVER_URL , data = data)
+    return_obj = json.loads(res.content)
+
+    videos = return_obj['all'] if return_obj['status'] == "success" else []
+    categories = return_obj['category'] if return_obj['status'] == "success" else []
     videos = decode_Video(videos)
-    list_search = []
-    for video in videos:
-            if(q in video.vid_title.lower()):
-                list_search.append(video)
+    categories = decode_Cate(categories)
     #Paginator
-    p = Paginator(list_search, 8)
+    p = Paginator(videos, 8)
     page = request.GET.get('page') 
     list_res = p.get_page(page)
     nums = "a" * list_res.paginator.num_pages
@@ -65,19 +97,34 @@ def managervideo(request , pk, cat):
 
 @login_required(login_url='/login')
 def addTv(request, pk, cat):
+    postObj = {
+        'method_name': 'LOAD_CATEGORT',
+        'type':pk,
+        'search_txt': ''
+    }
+
+    data = {
+        'data': json.dumps(postObj)
+    }
+
+    res = requests.post(SERVER_URL , data = data)
+    return_obj = json.loads(res.content)
+
+    categories = return_obj['category'] if return_obj['status'] == "success" else []
+    categories = decode_Cate(categories)
     if request.method == 'POST':
 
         if 'file_img' in request.FILES:
             print("update image")
 
-            url = 'http://localhost/watchvideoapp/uploadImage.php'
+            url = SERVER_URL + '/uploadImage.php'
             files = {'file': request.FILES['file_img']}
             data = {'crr': request.POST.get('thumbnail')}
             res = rq.post(url, files=files, params=data)
         
             js = json.loads(res.content)
             if(js['status'] == 1):
-                image_dir = 'http://localhost/watchvideoapp/image/'+js['dir'];
+                image_dir = SERVER_URL + '/image/'+js['dir'];
                 thumbnail = base64.b64encode(image_dir.encode('utf-8')).decode('utf-8')
             else:
                 messages.error(request, "Something wrong when upload image, please try again!")
@@ -86,21 +133,30 @@ def addTv(request, pk, cat):
             messages.error(request, "Thumbnail is required!")
             return redirect('videoapp:create')
 
-        video_new = {'cat': TblCategory.objects.get(cat_id=request.POST.get('category_vid')),
-        'vid_title': bs.encode_Str(request.POST.get('title')), 'vid_url': bs.encode_Str(request.POST.get('url')), 
-        'vid_thumbnail': thumbnail,
-        'vid_description': bs.encode_Str(request.POST.get('description')), 'vid_view': "0", 
-        'vid_duration': "0", 'vid_time': request.POST.get('time'), 'vid_avg_rate': "0", 
-        'vid_status': request.POST.get('vid_status'), 'vid_type': pk, 'vid_is_premium': "0"}
-        form = VideoForm(video_new)
-        if form.is_valid():
-            form.save()
+        postObj =  {
+                    'method_name': 'ADD_VIDEO',
+                    'cat_id':request.POST.get('category_vid'),
+                    'vid_title': bs.encode_Str(request.POST.get('title')),
+                    'vid_url': bs.encode_Str(request.POST.get('url')), 
+                    'vid_thumbnail': thumbnail,
+                    'vid_description': bs.encode_Str(request.POST.get('description')), 
+                    'vid_view': "0", 
+                    'vid_duration': "0", 
+                    'vid_time': request.POST.get('time'), 
+                    'vid_avg_rate': "0", 
+                    'vid_status': request.POST.get('vid_status'), 
+                    'vid_type': pk,
+                    'vid_is_premium': "0"}
+        data = {
+            'data': json.dumps(postObj)
+        }
+        res = requests.post(SERVER_URL , data = data)
+        return_obj = json.loads(res.content)
+        if str(return_obj) == "success":
             return redirect("managervideo:managervideo", pk, request.POST.get('category_vid'))
         else:
             return HttpResponse("Error")
-    else:
-        categories = TblCategory.objects.filter(Q(cat_type=pk))
-        categories = decode_Cate(categories)    
+    else:   
         date_time = date.today().strftime("%Y-%m-%d")
         page = 'add'
         context = {'page': page, 'choice':pk, 'cat':cat, 'date_time':date_time, 'categories':categories, 'mode': 'create'}
@@ -108,10 +164,24 @@ def addTv(request, pk, cat):
 
 @login_required(login_url='/login')
 def editTv(request, pk, cat, id):
-    video = TblVideo.objects.get(vid_id = id)
+    video = getVid(id)
     video = decode_Item_Video(video)
-    date_time = video.vid_time.strftime("%Y-%m-%d")
-    categories = TblCategory.objects.filter(Q(cat_type=pk))
+    date_time = (datetime.strptime(video['vid_time'], "%Y-%m-%d %H:%M:%S")).strftime('%Y-%m-%d')
+
+    postObj = {
+        'method_name': 'LOAD_CATEGORT',
+        'type':pk,
+        'search_txt': ''
+    }
+
+    data = {
+        'data': json.dumps(postObj)
+    }
+
+    res = requests.post(SERVER_URL , data = data)
+    return_obj = json.loads(res.content)
+
+    categories = return_obj['category'] if return_obj['status'] == "success" else []
     categories = decode_Cate(categories)
 
     if request.method == 'POST':
@@ -122,29 +192,40 @@ def editTv(request, pk, cat, id):
         if 'file_img' in request.FILES:
             print("update imageeeeeeeeeeeeeeeeeee")
 
-            url = 'http://localhost/watchvideoapp/uploadImage.php'
+            url = SERVER_URL + '/uploadImage.php'
             files = {'file': request.FILES['file_img']}
             data = {'crr': request.POST.get('thumbnail')}
             res = rq.post(url, files=files, params=data)
         
             js = json.loads(res.content)
             if(js['status'] == 1):
-                image_dir = 'http://localhost/watchvideoapp/image/'+js['dir'];
+                image_dir = SERVER_URL + '/image/'+js['dir'];
                 thumbnail = base64.b64encode(image_dir.encode('utf-8')).decode('utf-8')
             else:
                 messages.error(request, "Something wrong when upload image, please try again!")
                 return redirect('managervideo:editvideo', cat=cat, id=id)
 
-        video_new = {'vid_id': video.vid_id, 'cat': TblCategory.objects.get(cat_id=request.POST.get('category_vid')),
-        'vid_title': bs.encode_Str(request.POST.get('title')), 'vid_url': bs.encode_Str(request.POST.get('url')), 
-        'vid_thumbnail': thumbnail,
-        'vid_description': bs.encode_Str(request.POST.get('description')), 
-        'vid_view': request.POST.get('views'), 'vid_duration': "0", 'vid_time': request.POST.get('time'),
-        'vid_avg_rate': request.POST.get('rate'), 'vid_status': request.POST.get('vid_status'), 'vid_type': pk,
-        'vid_is_premium': "0"}
-        form = VideoForm(video_new, instance=video)
-        if form.is_valid():
-            form.save()
+        postObj = {
+            'method_name': 'UPDATE_VIDEO','vid_id': video['vid_id'],
+            'cat_id': request.POST.get('category_vid'),
+            'vid_title': bs.encode_Str(request.POST.get('title')), 
+            'vid_url': bs.encode_Str(request.POST.get('url')), 
+            'vid_thumbnail': thumbnail,
+            'vid_description': bs.encode_Str(request.POST.get('description')), 
+            'vid_view': request.POST.get('views'),
+            'vid_duration': video['vid_duration'],
+            'vid_time': request.POST.get('time'),
+            'vid_avg_rate': request.POST.get('rate'),
+            'vid_status': request.POST.get('vid_status'), 
+            'vid_type': pk,
+            'vid_is_premium': "0"}
+
+        data = {
+            'data': json.dumps(postObj)
+        }
+        res = requests.post(SERVER_URL , data = data)
+        return_obj = json.loads(res.content)
+        if str(return_obj) == "success":
             return redirect("managervideo:managervideo", pk, request.POST.get('category_vid'))
         else:
             return HttpResponse("Error")
@@ -155,14 +236,15 @@ def editTv(request, pk, cat, id):
 
 @login_required(login_url='/login')
 def disableVideo(request, pk, cat, id):
-    video = TblVideo.objects.get(vid_id=id)
-    video.vid_status = 0 if video.vid_status == 1 else 1
-    context = {'vid_id': video.vid_id, 'cat': video.cat, 'vid_title': video.vid_title, 
-    'vid_url': video.vid_url, 'vid_thumbnail': video.vid_thumbnail, 'vid_description':video.vid_description,
-    'vid_view': video.vid_view, 'vid_duration': video.vid_duration, 'vid_time': video.vid_time,
-    'vid_avg_rate': video.vid_avg_rate, 'vid_status': video.vid_status, 'vid_type': video.vid_type,
-    'vid_is_premium': video.vid_is_premium}
-    form = VideoForm(context, instance=video)
-    if form.is_valid():
-        form.save()
+    video = getVid(id)
+    postObj = {
+        'method_name': 'UPDATE_STATUS_RADIOTV',
+        'vid_id': id,
+        'status': 1 if video['vid_status'] == '0' else 0
+    }
+
+    data = {
+        'data': json.dumps(postObj)
+    }
+    res = requests.post(SERVER_URL , data = data)
     return redirect("managervideo:managervideo", pk, cat)
