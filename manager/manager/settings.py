d7141926ec2419bdd7565c61539fd24ab69b35c6
Do Thanh Tuan <81170150+tuanit1@users.noreply.github.com>
2022-06-05 10:43:06 +0700

    merge heroku to main

--- manager/manager/settings.py
+++ manager/manager/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s!v9ev8m9n#(de@!p02k^id$@y@-=y!yi&&3q_#$ht65tq!73d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'manager-watchvideoapp.herokuapp.com']
 
 
 # Application definition
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,6 +126,8 @@ USE_TZ = True
 
 MEDIA_URL = '/styles/images/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'home/media/')
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
