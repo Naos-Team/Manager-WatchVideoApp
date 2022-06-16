e958eb73f352b5189e928a1b719b66d82f4680c9
yennhiluu2112 <yennhiluu2112@gmail.com>
2022-06-16 11:19:04 +0700

    nhi cm: UI Login

--- manager/manager/settings.py
+++ manager/manager/settings.py
@@ -127,13 +127,13 @@ USE_TZ = True
 MEDIA_URL = '/styles/images/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'home/media/')
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'staticfiles/'
 
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'staticfiles'
-# ]
+STATICFILES_DIRS = [
+     os.path.join(BASE_DIR, "staticfiles"),
+]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
