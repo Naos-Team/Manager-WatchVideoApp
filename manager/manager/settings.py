832d4dd7154b4966b2a86fcdaee94668da6d4e31
Do Thanh Tuan <81170150+tuanit1@users.noreply.github.com>
2022-06-22 08:58:11 +0700

    tuan commit

--- manager/manager/settings.py
+++ manager/manager/settings.py
@@ -128,12 +128,13 @@ MEDIA_URL = '/styles/images/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'home/media/')
 
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = 'staticfiles/'
+# STATIC_URL = 'staticfiles/'
+
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT=os.path.join(BASE_DIR,"/staticfiles/")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"),)
 
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'staticfiles'
-]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
